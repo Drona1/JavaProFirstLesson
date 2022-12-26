d7463ea88a6a70ab9aa3e107f0cb10a9fac417a9
Drona <53328757+Drona1@users.noreply.github.com>
2022-12-27 01:16:31 +0200

    homework1

--- src/main/java/com/gmail/dimabah/javapro/homeworks/first/HelloServlet.java
+++ src/main/java/com/gmail/dimabah/javapro/homeworks/first/HelloServlet.java
@@ -5,14 +5,17 @@ import java.io.*;
 import jakarta.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
         String template = "<html><head><title>JavaProHomeworks</title>" +
                 "<link rel=\"stylesheet\" href=\"style.css\"></head>" +
                 "<body><h1>Hello, %s</h1></body></html>";
         String name = req.getParameter("name");
         resp.setContentType("text/html");
+
         try (PrintWriter pw = resp.getWriter()) {
             pw.println(String.format(template, name));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
